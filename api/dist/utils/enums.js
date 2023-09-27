77465e1fd12c19d49433d4900cb1cc469c635dfe
Pedro Prado <pedropradon@gmail.com>
2023-09-27 16:06:16 -0300

    remove node modules and dist

--- api/dist/utils/enums.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// Declaração de enums para garantir valores que podem ser inseridos em alguns campos do banco de dados
-Object.defineProperty(exports, "__esModule", { value: true });
